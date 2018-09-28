8d7cfafaaca084df9e58ecbaabf063b6a62d9a1a
luljam <lucianofdebrito@gmail.com>
2018-09-28 17:38:55 -0300

    first commit - Login

---
+++ src/hspm/util/DAOFactory.java
@@ -0,0 +1,16 @@
+package hspm.util;
+
+
+import hspm.usuario.UsuarioDAO;
+import hspm.usuario.UsuarioDAOHibernate;
+
+public class DAOFactory {
+
+	public static UsuarioDAO criarUsuarioDAO() {
+		UsuarioDAOHibernate usuarioDAO = new UsuarioDAOHibernate();
+		usuarioDAO.setSession(HibernateUtil.getSessionFactory().getCurrentSession());
+		return usuarioDAO;
+	}
+
+	
+}
